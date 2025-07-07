1faf47b56ba31fd0687ba8efb85bf9dd95b50fc4
mrmarkuz <mrmarkuz666@gmail.com>
2025-07-07 20:31:14 +0200

    refactor: replaced all kickstart names with planka

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="planka"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-planka container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-planka; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-planka -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-planka \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
